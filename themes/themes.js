b0b5591b9a8e17bcc06f8f38919d558bd1094a9c
Elias Ruiz Hz <conk_soldiers@hotmail.com>
2014-05-09 10:14:38 -0500

    Create themes.js

---
+++ themes/themes.js
@@ -0,0 +1,27 @@
+.theme_blue{
+	background: #eee;
+}h1.theme_blue,h2.theme_blue{
+	background: transparent;
+	color: #444;
+	text-shadow: 0 0 1px #000;
+}h3.theme_blue,h4.theme_blue{
+	background: #271859;
+	color: #fff;
+	text-shadow:0 -1px 1px #aaa;
+	padding: 5px;
+	padding-top: 10px; 
+}h5.theme_blue,h6.theme_blue{
+	background: transparent;
+	color: #444;
+}p.theme_blue{
+	background: #f8f8f8;
+	border: 1px #cacaca solid;
+	font-size: 13px;
+	line-height: 19px;
+	padding: 6px 10px;
+	border-radius: 3px;
+	overflow: auto;
+	color: #333;
+}b.theme_blue{
+	background: #f0f0f0;
+}
